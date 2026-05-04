fc7eac5587430000f06812b6f90e4bbff975f43f
Sai Srikar Emani <esrikar01@gmail.com>
2026-05-04 17:25:52 -0500

    Add: add-binary - submission-1

---
+++ Data Structures & Algorithms/add-binary/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        java.math.BigInteger numA = new java.math.BigInteger(a, 2);
+        java.math.BigInteger numB = new java.math.BigInteger(b, 2);
+
+        System.out.println(numA);
+        System.out.println(numB);
+
+        String res = numA.add(numB).toString(2);
+
+        return res;
+    }
+}

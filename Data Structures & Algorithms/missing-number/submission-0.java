4ae6966b982ac4918a7666f781a48270a6ac5eaf
Sai Srikar Emani <esrikar01@gmail.com>
2026-05-06 22:19:00 -0500

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        //Find sum of 0 to nums.length
+        //Then calc. sum of each ele in nums
+        //Our missing number = total range sum - actaul sum
+
+        int totalSum = 0;
+        for(int num = 0; num <= nums.length; num++){
+            totalSum += num;
+        }
+
+        System.out.println("Total Sum: " + totalSum);
+
+        int actualSum = 0;
+        for(int num : nums){
+            actualSum += num;
+        }
+
+        System.out.println("Actual Sum: " + actualSum);
+
+        return totalSum - actualSum;
+
+    }
+}

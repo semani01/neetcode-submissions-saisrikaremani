b7ce02b240d796596cdf9dbf357942e3a97e8841
Sai Srikar Emani <esrikar01@gmail.com>
2026-05-13 11:04:27 -0500

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int mid;
+
+        while(l <= r){
+            mid = (l + r) / 2;
+
+            if (target > nums[mid]){
+                l = mid + 1;
+            } else if (target < nums[mid]){
+                
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        
+        
+        return l;
+    }
+}

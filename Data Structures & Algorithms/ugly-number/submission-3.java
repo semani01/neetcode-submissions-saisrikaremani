88796419ed4197169b5f2110028e4def517cca49
Sai Srikar Emani <esrikar01@gmail.com>
2026-05-18 10:46:26 -0500

    Add: ugly-number - submission-3

---
+++ Data Structures & Algorithms/ugly-number/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 1){
+            return true;
+        }
+
+        int[] factors = {2, 3, 5};
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+
+        return n == 1;
+    }
+}
